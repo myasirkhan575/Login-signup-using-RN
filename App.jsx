04df6219b91863badf7c01fe3465acd74a97b646
myasirkha575 <myasirkhan575@gmail.com>
2020-07-05 01:43:43 +0500

    login

---
+++ App.jsx
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React  from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TextInput,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+// import { txtinp } from './styles'
+
+const App = (props) => {
+ 
+  
+   
+ 
+  return (
+    <SafeAreaView>
+      <ScrollView>
+      <View>
+      <TextInput />
+      
+    </View>
+      </ScrollView>
+    </SafeAreaView>
+   
+    
+
+
+
+  );
+};
+    
+export default App;
---
+++ styles.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Dimensions } from 'react-native';
+import { color } from 'react-native-reanimated';
+import App from './App';
+
+
+const txtinp = StyleSheet.create({
+    inp: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginTop: 150,
+        }
+})
+   
+
+
+
+
+
+export
+{
+    txtinp
+}
